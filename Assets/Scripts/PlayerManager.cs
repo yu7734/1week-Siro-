80794b9f25ec2a84ec1fb21572e96ab7d171323d
yu7734 <yuichi.7734@gmail.com>
2026-07-30 01:13:23 +0900

    移動範囲を設定

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,6 +6,11 @@ public class PlayerManager : MonoBehaviour
     [SerializeField, Tooltip("プレイヤーの移動速度")] private float playerMoveSpeed;
     private CharacterController characterController;
     private PlayerInputScript playerInputScript;
+    [Header("プレイヤーの移動範囲")]
+    [SerializeField, Tooltip("横移動の最小")] private float minPlayerRangeX;
+    [SerializeField, Tooltip("横移動の最大")] private float maxPlayerRangeX;
+    [SerializeField, Tooltip("縦移動の最小")] private float minPlayerRangeY;
+    [SerializeField, Tooltip("縦移動の最大")] private float maxPlayerRangeY;
 
     private void Awake()
     {
@@ -28,7 +33,11 @@ public class PlayerManager : MonoBehaviour
     {
         //入力に応じて移動
         var moveVelocity = new Vector3(playerInputScript.GetSetInputMove.x * playerMoveSpeed, playerInputScript.GetSetInputMove.y * playerMoveSpeed, 0);
-
         characterController.Move(moveVelocity * Time.deltaTime);
+
+        Vector3 currentPosition = this.transform.position;//現在の位置
+        currentPosition.x = Mathf.Clamp(currentPosition.x, minPlayerRangeX, maxPlayerRangeX);//X軸の移動範囲
+        currentPosition.y = Mathf.Clamp(currentPosition.y, minPlayerRangeY, maxPlayerRangeY);//Y軸の移動範囲
+        this.transform.position = currentPosition;//現在の位置をcurrentPositionにする
     }
 }
