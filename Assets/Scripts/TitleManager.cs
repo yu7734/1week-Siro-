0406896d97fd624afed3fcdb17392ae0e59b3bfd
yu7734 <yuichi.7734@gmail.com>
2026-08-02 01:11:11 +0900

    タイトルシーンを追加

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
