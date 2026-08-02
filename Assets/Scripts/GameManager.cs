42c578f22eaa762ea05aa7243c389cb357db0319
yu7734 <yuichi.7734@gmail.com>
2026-08-02 15:44:53 +0900

    unityroomのスコアランキング機能を実装

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using unityroom.Api;
 
 public class GameManager : MonoBehaviour
 {
@@ -24,7 +25,8 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         Time.timeScale = 0;
-        gameOverText.text = materCounter.GetSetMaterCount.ToString("F2") + "M先見たものとは？！";
+        gameOverText.text = materCounter.GetSetMaterCount.ToString("F2") + "M先見たものとは？！";//テキスト表示
+        UnityroomApiClient.Instance.SendScore(1, materCounter.GetSetMaterCount, ScoreboardWriteMode.HighScoreDesc);//スコアをunityroomのランキングに送る
         for (int i = 0; i < gameOverUIObjects.Length; ++i)
             gameOverUIObjects[i].SetActive(true);
     }
